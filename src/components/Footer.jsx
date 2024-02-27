e8f7dc01e6f6f1f101264971615ba99491b5dcdb
dnlsilv <danielsilvacamacho02@gmail.com>
2024-02-27 12:36:29 +0100

    Primer commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (  
+        <footer>
+            <p className="author">🪐 by <a href="https://github.com/dnlsilv" target="_blank">Jorge Romero, Daniel Silva</a></p>
+        </footer>
+    );
+}
+ 
+export default Footer;
